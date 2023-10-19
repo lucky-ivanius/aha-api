98fda71639ce1800d4e45a05ebb7b2222be1464c
Lucky Ivanius <luckzivanius@gmail.com>
2023-10-19 19:21:04 +0700

    Fix swagger ui to use custom css

--- src/infrastructure/http/api/routes/docs/index.ts
+++ src/infrastructure/http/api/routes/docs/index.ts
@@ -1,11 +1,8 @@
-import { RequestHandler } from 'express-serve-static-core';
 import { serve, setup } from 'swagger-ui-express';
 import docFile from '../../../docs/index.json';
 
-export const swaggerRender: RequestHandler = (req, res, next) => {
-  res.setHeader('Content-Type', 'text/html;charset=utf-8');
+const customCssUrl =
+  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
-  next();
-};
 export const swaggerServe = serve;
-export const swaggerSetup = setup(docFile);
+export const swaggerSetup = setup(docFile, { customCssUrl });
