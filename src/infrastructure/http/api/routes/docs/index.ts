0b911c96006d6690f1735db8940fd641dcd2697c
Lucky Ivanius <luckzivanius@gmail.com>
2023-10-19 19:37:29 +0700

    Fix swagger ui version

--- src/infrastructure/http/api/routes/docs/index.ts
+++ src/infrastructure/http/api/routes/docs/index.ts
@@ -1,8 +1,5 @@
 import { serve, setup } from 'swagger-ui-express';
 import docFile from '../../../docs/index.json';
 
-const customCssUrl =
-  'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
-
 export const swaggerServe = serve;
-export const swaggerSetup = setup(docFile, { customCssUrl });
+export const swaggerSetup = setup(docFile);
