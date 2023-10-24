97c7cacf534a47fdc2e169ec4cd3e593e6f898a7
Lucky Ivanius <luckzivanius@gmail.com>
2023-10-25 04:06:13 +0700

    Fix auth0 user sync issues on login flow actions

--- src/infrastructure/di/services/auth0.service.ts
+++ src/infrastructure/di/services/auth0.service.ts
@@ -1,8 +1,10 @@
 import { auth0Config } from '../../config/auth0.config';
 import { Auth0Service } from '../../services/auth0.service';
+import { jwtService } from './jwt.service';
 
 export const auth0Service = new Auth0Service(
   auth0Config.domain,
   auth0Config.clientId,
-  auth0Config.clientSecret
+  auth0Config.clientSecret,
+  jwtService
 );
--- src/infrastructure/services/auth0.service.ts
+++ src/infrastructure/services/auth0.service.ts
@@ -1,39 +1,47 @@
-import { ManagementClient, UserInfoClient } from 'auth0';
+import { ManagementClient } from 'auth0';
 import { Auth0Metadata } from '../../application/dtos/external/auth0/auth0-metadata.dto';
 import { ExternalAuthService } from '../../application/services/external-auth.service';
+import {
+  Payload,
+  TokenService,
+} from '../../application/services/token.service';
 import { Id } from '../../domain/common/id';
 import { Email } from '../../domain/models/user/email';
 import { IdentityProvider } from '../../domain/models/user/identity-provider';
 import { Name } from '../../domain/models/user/name';
 import { User } from '../../domain/models/user/user';
 
+interface Auth0UserPayload extends Payload {
+  email: string;
+  email_verified: boolean;
+  name: string;
+}
+
 export class Auth0Service implements ExternalAuthService {
   public readonly providerName = 'auth0';
 
   private readonly managementClient: ManagementClient;
 
-  private readonly userInfoClient: UserInfoClient;
-
   constructor(
     readonly domain: string,
     readonly clientId: string,
-    readonly clientSecret: string
+    readonly clientSecret: string,
+    private readonly jwtService: TokenService
   ) {
     this.managementClient = new ManagementClient({
       domain,
       clientId,
       clientSecret,
     });
-    this.userInfoClient = new UserInfoClient({
-      domain,
-    });
   }
 
   async getUserByToken(accessToken: string): Promise<User | null> {
     try {
-      const result = await this.userInfoClient.getUserInfo(accessToken);
+      const user = (await this.jwtService.verify(
+        accessToken
+      )) as Auth0UserPayload;
 
-      const user = result.data;
+      if (!user) return null;
 
       const allowChangePassword = user.sub.startsWith('auth0');
 
